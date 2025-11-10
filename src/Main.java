736f85f7c581f9aafc60747665f8f58d84c49126
NATO-dotcom <nato114267@students.must.ac.ke>
2025-11-10 20:28:11 +0300

    Java basics practice

---
+++ src/Main.java
@@ -0,0 +1,4 @@
+
+void main() {
+  System.out.println("Hello Jude, I'm using JDK 25");
+}
---
+++ src/loops.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+void main() {
+    String[]fruits = {"Kiwi, Cherry, PineApple, Mango, Orange"};
+
+    int numOfFruits = fruits.length;
+
+    System.out.println(numOfFruits);
+
+}
